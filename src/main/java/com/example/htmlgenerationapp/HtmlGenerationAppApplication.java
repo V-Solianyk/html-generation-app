974c4cee3845d15c189e234260162cadf76bfddd
V-Solianyk <112614843+V-Solianyk@users.noreply.github.com>
2023-10-04 17:15:25 +0300

    initial the project.

---
+++ src/main/java/com/example/htmlgenerationapp/HtmlGenerationAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.htmlgenerationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmlGenerationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtmlGenerationAppApplication.class, args);
+	}
+
+}
